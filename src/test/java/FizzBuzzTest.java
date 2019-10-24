b2bbce2e6dbc82be5d2609c129e42f6cbc2e82a0
Marco Pfiffner <pfiffner90@gmail.com>
2019-10-24 13:21:29 +0200

    Add initial project files

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void initializeProject() {
+        // given
+
+        // when
+
+        // then
+        assertTrue(true);
+    }
+
+}
