3bf397562709b77c76ff8c009cd0241484c3a127
Marco Pfiffner <pfiffner90@gmail.com>
2019-10-24 14:18:22 +0200

    Implement fizzbuzz tdd style

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,26 @@
 public class FizzBuzz {
+
+    public String processNumber(int input) {
+        if (isModulo3(input) && isModulo5(input)) {
+            return "FizzBuzz";
+        }
+
+        if (isModulo3(input)) {
+            return "Fizz";
+        }
+
+        if (isModulo5(input)) {
+            return "Buzz";
+        }
+
+        return String.valueOf(input);
+    }
+
+    private boolean isModulo3(int input) {
+        return input % 3 == 0;
+    }
+
+    private boolean isModulo5(int input) {
+        return input % 5 == 0;
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
@@ -11,7 +12,102 @@ public class FizzBuzzTest {
         // when
 
         // then
-        assertTrue(true);
+        assertThat(true, is(true));
     }
 
+    @Test
+    public void shouldProcessInput() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String output = fizzBuzz.processNumber(1);
+
+        // then
+        assertThat(output, is("1"));
+    }
+
+    @Test
+    public void shouldProcessInput_2() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String output = fizzBuzz.processNumber(2);
+
+        // then
+        assertThat(output, is("2"));
+    }
+
+    @Test
+    public void shouldProcessInput_3() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String output = fizzBuzz.processNumber(3);
+
+        // then
+        assertThat(output, is("Fizz"));
+    }
+
+    @Test
+    public void shouldProcessInput_4() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String output = fizzBuzz.processNumber(4);
+
+        // then
+        assertThat(output, is("4"));
+    }
+
+    @Test
+    public void shouldProcessInput_5() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String output = fizzBuzz.processNumber(5);
+
+        // then
+        assertThat(output, is("Buzz"));
+    }
+
+    @Test
+    public void shouldProcessInput_6() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String output = fizzBuzz.processNumber(6);
+
+        // then
+        assertThat(output, is("Fizz"));
+    }
+
+    @Test
+    public void shouldProcessInput_10() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String output = fizzBuzz.processNumber(10);
+
+        // then
+        assertThat(output, is("Buzz"));
+    }
+
+    @Test
+    public void shouldProcessInput_15() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String output = fizzBuzz.processNumber(15);
+
+        // then
+        assertThat(output, is("FizzBuzz"));
+    }
 }
